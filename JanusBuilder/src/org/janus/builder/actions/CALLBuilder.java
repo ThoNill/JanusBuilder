27ce7b546729393f9f6d648d3218f99266f46792
Thomas Nill <t.nill@t-online.de>
2016-03-22 20:59:57 +0100

    Logger hinzugefuegt

--- JanusBuilder/src/org/janus/builder/actions/CALLBuilder.java
+++ JanusBuilder/src/org/janus/builder/actions/CALLBuilder.java
@@ -1,10 +1,15 @@
 package org.janus.builder.actions;
 
+import org.apache.logging.log4j.LogManager;
+
+import org.apache.logging.log4j.Logger;
 import org.janus.actions.Action;
 import org.janus.bean.CallDataValue;
 import org.jdom2.Element;
 
 public class CALLBuilder extends DefaultBuilderAction {
+	private static final Logger LOG = LogManager.getLogger(CALLBuilder.class);
+	
 	BEANBuilder builder;
 
 	public CALLBuilder(BEANBuilder builder) {
@@ -14,11 +19,28 @@ public class CALLBuilder extends DefaultBuilderAction {
 
 	@Override
 	protected Action createAction(Element elem) {
+		LOG.debug("create Call Action");
 		String command = elem.getAttributeValue("command");
 		String name = elem.getAttributeValue("name");
 		
 		CallDataValue call =  new CallDataValue(builder.getBean(),command);
-			
+		
+		for (Element set : elem.getChildren()) {
+			if ("SET".equals(set.getName())) {
+				String varAttribute = set.getAttributeValue("var");
+				String toAttribute = set.getAttributeValue("to");
+				String constAttribute = set.getAttributeValue("constant");
+				if (toAttribute != null) {
+					LOG.debug("add Var setter {} {}",varAttribute,toAttribute);
+					call.addSetterValue(varAttribute,toAttribute);
+				}
+				if (constAttribute != null) {
+					LOG.debug("add const setter {} {}",varAttribute,constAttribute);
+					call.addSetterConstant(varAttribute,constAttribute);
+				}
+			}
+		}
+		LOG.debug("Action created");	
 		return call;
 	}
 }
